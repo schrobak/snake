8df4aeb21efa51b604af72bf310ab35fbe6df884
Slawomir Chrobak <slawomir.chrobak@gmail.com>
2019-10-15 00:04:03 +0200

    Remove old html

--- src/styles.ts
+++ src/styles.ts
@@ -3,9 +3,25 @@ import { normalize } from "styled-normalize";
 
 export const GlobalStyle = createGlobalStyle`
   ${normalize}
+  
+  * {
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    background-color: #000;
+    height: 100%;
+  }
 
   body {
-    padding: 0;
-    background-color: black;
+    background: #222;
+    background: radial-gradient(#333, #111);
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    color: #fff;
+    font: 100%/1.5 sans-serif;
+    overflow: hidden;
   }
 `;
