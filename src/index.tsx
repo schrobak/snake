9b2aeb63c4fb8b786cfa5feb7a9b8233763d6eb6
Slawomir Chrobak <slawomir.chrobak@gmail.com>
2019-10-14 23:58:17 +0200

    Move to styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 import App from "./App";
+import { GlobalStyle } from "./styles";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Root = () => (
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+);
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
