1bb78d21aa82180d3d7e822d548ca55459dc92c4
Slawomir Chrobak <slawomir.chrobak@gmail.com>
2019-10-20 14:00:12 +0200

    Fix board size calculation

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -33,8 +33,8 @@ interface BoardProps {
 }
 
 const Board = styled.div<BoardProps>`
-  width: ${({ columns, tileSize }) => columns * tileSize + (tileSize - 1)}px;
-  height: ${({ rows, tileSize }) => rows * tileSize + (tileSize - 1)}px;
+  width: ${({ columns, tileSize }) => columns * tileSize + (columns - 1)}px;
+  height: ${({ rows, tileSize }) => rows * tileSize + (rows - 1)}px;
   position: relative;
   display: grid;
   grid-template-rows: repeat(${props => props.rows}, ${props => props.tileSize}px);
